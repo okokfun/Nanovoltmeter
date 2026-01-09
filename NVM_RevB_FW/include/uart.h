4289962724f25d306910915ed6f22e594daf8be7
Fred·Lin <2643989637@qq.com>
2026-01-10 00:20:43 +0800

    更新 uart.h

--- NVM_RevB_FW/include/uart.h
+++ NVM_RevB_FW/include/uart.h
@@ -1,7 +1,5 @@
-
 #ifndef UART_H_
 #define UART_H_
-
 
 #include "stm32u5xx.h"
 #include "stm32u575xx.h"
@@ -9,8 +7,6 @@
 #include "dma.h"
 #include "sys_cfg.h"
 #include <stdint.h>
-
-
 
 #define UART_RX_CMD_SIZE					64		// Length in bytes of a command
 #define CMD_BUFFER_LEN						8		// Min number of commands that can fit in buffer
@@ -21,7 +17,6 @@
 extern volatile char uart_tx_buffer[UART_TX_BUFFER_SIZE];
 extern volatile char uart_rx_buffer[UART_RX_BUFFER_SIZE];
 
-
 void USART3Init(void);
 void USART3DisableDMATx(void);
 void USART3EnableDMATx(void);
@@ -31,6 +26,5 @@ void sendDataBlocking(char * buf, uint16_t len);
 void USART3SendChar(uint8_t c);
 
 void sendData(char * buf, uint8_t len);
-
 
 #endif // UART_H_
