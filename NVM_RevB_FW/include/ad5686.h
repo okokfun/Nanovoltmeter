5ecb073c12e4813e0b1632e34aa74006f802514e
Fred·Lin <2643989637@qq.com>
2026-01-10 00:39:19 +0800

    更新 ad5686.h

--- NVM_RevB_FW/include/ad5686.h
+++ NVM_RevB_FW/include/ad5686.h
@@ -1,5 +1,4 @@
-	
-	/*	Basic driver for AD5686 in NVM Rev B
+/*	Basic driver for AD5686 in NVM Rev B
 	*
 	*
 	*/
@@ -13,7 +12,7 @@
 
 /* Note: Command defines are aligned with the first byte of the transfer */
 
-	/* COMMAND DEFINES */
+/* COMMAND DEFINES */
 #define AD5686_WRITE_DAC			0x01 << 4
 #define AD5686_UPDATE_DAC			0x02 << 4
 #define AD5686_WRITE_UPDATE		0x03 << 4
@@ -24,7 +23,7 @@
 #define AD5686_READBACK				0x09 << 4
 #define AD5686_NO_OP					0x0F << 4
 
-	/* ADDRESS DEFINES */
+/* ADDRESS DEFINES */
 typedef enum{
 	AD5686_DAC_A					= (1U << 0),
 	AD5686_DAC_B					= (1U << 1),
@@ -32,19 +31,19 @@ typedef enum{
 	AD5686_DAC_D					= (1U << 3)
 }AD5686Channel_t;
 
-	/* OUTPUT DEFINES */
+/* OUTPUT DEFINES */
 #define MOD1_HI_DAC					AD5686_DAC_A	// Output A
 #define V_OS_DAC					AD5686_DAC_B	// Output B
 #define MOD2_HI_DAC					AD5686_DAC_C	// Output C
 #define MOD_LO_DAC					AD5686_DAC_D	// Output D
 
-	/* REPEAT USE DEFINES */
+/* REPEAT USE DEFINES */
 #define AD5686_WRT_UP_DAC_A		(AD5686_WRITE_UPDATE | AD5686_DAC_A)
 #define AD5686_WRT_UP_DAC_B		(AD5686_WRITE_UPDATE | AD5686_DAC_B)
 #define AD5686_WRT_UP_DAC_C		(AD5686_WRITE_UPDATE | AD5686_DAC_C)
 #define AD5686_WRT_UP_DAC_D		(AD5686_WRITE_UPDATE | AD5686_DAC_D)
 
-	/* DEFAULT DAC OUTPUTS */
+/* DEFAULT DAC OUTPUTS */
 #define MOD1_HI_DEFAULT				65535U	// 2.50V
 #define MOD2_HI_DEFAULT				65535U	// 2.50V
 #define V_OS_DEFAULT					0x4B80
@@ -57,7 +56,6 @@ typedef struct{
 	DMA_Channel_TypeDef * dma;
 	uint32_t channel_codes[AD5686_NUM_CHANNELS];
 }AD5686_t;
-
 
 void ad5686_set_spi_bus(AD5686_t * dac, SPI_TypeDef * spi);
 void ad5686_write_dac(AD5686_t * dac, AD5686Channel_t ch, uint16_t code);
