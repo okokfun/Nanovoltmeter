b984a80111a5f3bb4c257303c93f4dfe948dc101
Fred·Lin <2643989637@qq.com>
2026-01-10 00:37:13 +0800

    更新 cmd_handler.h

--- NVM_RevB_FW/include/cmd_handler.h
+++ NVM_RevB_FW/include/cmd_handler.h
@@ -5,10 +5,11 @@
 #include "lexer.h"
 #include "serial_io.h"
 
-    /** In the annotations for each #define, a brief explanation is given
-     * about the node and a paranthetical notation of whether it can be
-     * a command (c) or query (q). If there is no c or q, then it is not
-     * a terminal node */
+/** In the annotations for each #define, a brief explanation is given
+ * about the node and a paranthetical notation of whether it can be
+ * a command (c) or query (q). If there is no c or q, then it is not
+ * a terminal node 
+*/
 
 CmdErr command_handler(CmdString_t * cmd);
 
