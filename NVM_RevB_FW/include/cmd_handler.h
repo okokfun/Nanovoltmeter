d24398e9dc1532fe2441dd40edf47874b1ec9e61
Fred·Lin <2643989637@qq.com>
2026-01-10 00:36:38 +0800

    更新 cmd_handler.h

--- NVM_RevB_FW/include/cmd_handler.h
+++ NVM_RevB_FW/include/cmd_handler.h
@@ -1,4 +1,3 @@
-
 #ifndef CMD_HANDLER_H_
 #define CMD_HANDLER_H_
 
@@ -10,8 +9,6 @@
      * about the node and a paranthetical notation of whether it can be
      * a command (c) or query (q). If there is no c or q, then it is not
      * a terminal node */
-
-
 
 CmdErr command_handler(CmdString_t * cmd);
 
